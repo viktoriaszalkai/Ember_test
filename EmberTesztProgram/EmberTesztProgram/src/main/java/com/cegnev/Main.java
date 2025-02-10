1dbb44e3c60cca88a430fe32b1d0a4cc923f5a85
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2025-02-10 16:04:24 +0100

    minden teszt lefut main

--- EmberTesztProgram/EmberTesztProgram/src/main/java/com/cegnev/Main.java
+++ EmberTesztProgram/EmberTesztProgram/src/main/java/com/cegnev/Main.java
@@ -1,8 +1,56 @@
 package com.cegnev;
 
-public class Main {
+import com.cegnev.modell.Jatekos;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Main implements Serializable {
     public static void main(String[] args) {
 
     }
 
+    public static void fajlbaIr(Path fajl, List<String> sorok) throws IOException {
+        Files.write(fajl, sorok, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    public static List<String> fajlbolOlvas(Path tempFile) {
+        List<String> sorok = null;
+        try {
+            sorok = Files.readAllLines(tempFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sorok;
+    }
+
+    public static void jatekosSorositasa(Jatekos eredeti) {
+            try (ObjectOutputStream objKi = new ObjectOutputStream(new FileOutputStream("jatekos.ser"))) {
+                objKi.writeObject(eredeti);
+            }catch (FileNotFoundException e){
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw  new RuntimeException();
+            }
+
+    }
+
+    public static Jatekos jatekosDeszerializalasa() {
+        Jatekos beJatekos;
+        try (ObjectInputStream objBe = new ObjectInputStream(new FileInputStream("jatekos.ser"))) {
+            beJatekos = (Jatekos) objBe.readObject();
+            System.out.println("A beolvasott játékos: ");
+            System.out.println(beJatekos);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return beJatekos;
+    }
 }
--- EmberTesztProgram/EmberTesztProgram/src/test/com/cegnev/MainTest.java
+++ EmberTesztProgram/EmberTesztProgram/src/test/com/cegnev/MainTest.java
@@ -1,59 +1,59 @@
-//package com.cegnev;
-//
-//import com.cegnev.modell.Jatekos;
-//import com.cegnev.modell.Kaszt;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class MainTest {
-//    @Test
-//    void testFajlbaIras() throws Exception {
-//        Path tempFile = Files.createTempFile("testFile", ".txt");
-//        try {
-//            List<String> sorok = List.of("Fájlba", "író", "teszt, 3 sorral.");
-//            Main.fajlbaIr(tempFile, sorok);
-//            assertTrue(Files.exists(tempFile));
-//
-//            List<String> beolvasottSorok = Files.readAllLines(tempFile);
-//            assertEquals(sorok, beolvasottSorok);
-//        } finally {
-//            Files.deleteIfExists(tempFile);
-//        }
-//    }
-//
-//    @Test
-//    void testFajlbolOlvasas() throws Exception {
-//        Path tempFile = Files.createTempFile("testFile", ".txt");
-//
-//        try {
-//            List<String> sorok = List.of("Line 1", "Line 2", "Line 3");
-//            Files.write(tempFile, sorok);
-//
-//            List<String> beolvasottSorok = Main.fajlbolOlvas(tempFile);
-//
-//            assertEquals(sorok, beolvasottSorok);
-//        } finally {
-//            Files.deleteIfExists(tempFile);
-//        }
-//    }
-//
-//    @Test
-//    void testSzerializacio() throws IOException {
-//        Jatekos eredeti = new Jatekos("Kübli", 23, Kaszt.HARCOS);
-//        Main.jatekosSorositasa(eredeti);
-//        assertTrue(Files.exists(Path.of("jatekos.ser")));
-//
-//        Jatekos deszerializalt = Main.jatekosDeszerializalasa();
-//
-//        assertEquals(eredeti, deszerializalt, "A deszerralizált objektumnak egyeznie kell az eredetivel.");
-//
-//    }
-//}
+package com.cegnev;
+
+import com.cegnev.modell.Jatekos;
+import com.cegnev.modell.Kaszt;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testFajlbaIras() throws Exception {
+        Path tempFile = Files.createTempFile("testFile", ".txt");
+        try {
+            List<String> sorok = List.of("Fájlba", "író", "teszt, 3 sorral.");
+            Main.fajlbaIr(tempFile, sorok);
+            assertTrue(Files.exists(tempFile));
+
+            List<String> beolvasottSorok = Files.readAllLines(tempFile);
+            assertEquals(sorok, beolvasottSorok);
+        } finally {
+            Files.deleteIfExists(tempFile);
+        }
+    }
+
+    @Test
+    void testFajlbolOlvasas() throws Exception {
+        Path tempFile = Files.createTempFile("testFile", ".txt");
+
+        try {
+            List<String> sorok = List.of("Line 1", "Line 2", "Line 3");
+            Files.write(tempFile, sorok);
+
+            List<String> beolvasottSorok = Main.fajlbolOlvas(tempFile);
+
+            assertEquals(sorok, beolvasottSorok);
+        } finally {
+            Files.deleteIfExists(tempFile);
+        }
+    }
+
+    @Test
+    void testSzerializacio() throws IOException {
+        Jatekos eredeti = new Jatekos("Kübli", 23, Kaszt.HARCOS);
+        Main.jatekosSorositasa(eredeti);
+        assertTrue(Files.exists(Path.of("jatekos.ser")));
+
+        Jatekos deszerializalt = Main.jatekosDeszerializalasa();
+
+        assertEquals(eredeti, deszerializalt, "A deszerralizált objektumnak egyeznie kell az eredetivel.");
+
+    }
+}
